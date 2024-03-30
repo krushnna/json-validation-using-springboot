559b084e9c42a999aff0cf773c15db092798b4d4
Krushnna Baviskar <krushnnabaviskar@gmail.com>
2024-03-30 14:33:04 +0530

    Update GenrativeConstructor.java

--- JsonValidatorApplication/src/main/java/com/microservices/jsonvalidatorapp/jsonvalidatorapplication/controller/GenrativeConstructor.java
+++ JsonValidatorApplication/src/main/java/com/microservices/jsonvalidatorapp/jsonvalidatorapplication/controller/GenrativeConstructor.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/chat")
 public class GenrativeConstructor {
 
